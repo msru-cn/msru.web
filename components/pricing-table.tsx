4d6a3d87df45952247884610da8d1885b219fd6e
msru <dev@msru.cn>
2026-07-11 17:49:01 +0800

    refactor(p0): use local cn util instead of @msru/ui/lib/utils

--- components/pricing-table.tsx
+++ components/pricing-table.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button, Switch } from "@msru/ui/components";
-import { cn } from "@msru/ui/lib/utils";
+import { cn } from "@/lib/cn";
 import { Check, Info, Minus } from "lucide-react";
 import { useState } from "react";
 
